25c7f8f242fb409b2e8c4c4fec67ce6248e7ffd7
gabriel <gvictor525.gv@gmail.com>
2019-04-23 11:07:53 -0300

    index interface maked

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ $(window).on('load', function() {
 
     $('#years').text(years);
 });
+
+$(window).on("scroll", function() {
+    var y = $(window).scrollTop();
+
+    //alert("ola");
+
+    if(y >= $(window).height() / 3) {
+        $("#menu").addClass("menu-style");
+        $("#menu a").css('color', '#000');
+    } else {
+        $("#menu").removeClass("menu-style");
+        $("#menu a").css('color', '#fff');
+    }
+});
